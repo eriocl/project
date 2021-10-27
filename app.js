0f134d48b18f9d4f82fc4607500ba5f22f3410d3
Alex <eriocl@ya.ru>
2021-10-27 12:39:46 +0300

    base structure added

---
+++ app.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const indexRouter = require("./routes/indexRouter");
+
+const app = express();
+const { PORT } = process.env;
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+app.use(express.static(path.join(process.env.PWD, "public")));
+
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.listen(PORT, () => {
+  console.log(`server started PORT: ${PORT}`);
+});
+
+module.exports = app;
