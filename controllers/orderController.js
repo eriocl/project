821b086a11b645ad15f7c37be55932eafa874350
Alex <eriocl@ya.ru>
2021-10-28 16:57:23 +0300

    Order store route added

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,8 +1,14 @@
+const { User, Order, Entry } = require('../db/models');
+
 class OrderController {
   static async store(req, res) {
+    res.sendStatus(200);
     try {
       console.log(req.body);
-      const {} = req.body;
+      const user = { ...req.body.user };
+      const order = { ...req.body.order };
+      const entry = { ...req.body.entry };
+      // const newUser = User
     } catch (e) {
       console.log(e);
       res.sendStatus(500);
