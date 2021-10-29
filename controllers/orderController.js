a7e6ee6465fb0d44f2f9bfe321139bde0cc009bf
Alex <eriocl@ya.ru>
2021-10-29 13:08:09 +0300

    admin panel added

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -4,14 +4,13 @@ const {
 
 class OrderController {
   static async store(req, res) {
-    
     try {
       const user = { ...req.body.user };
       const order = { ...req.body.order };
       const entry = { ...req.body.entry };
       const newUser = await User.create({ ...user });
       const newOrder = await Order.create({ ...order, user_id: newUser.id, paid: false });
-      const newEntry = await OrderEntry.create({
+      await OrderEntry.create({
         ...entry, order_id: newOrder.id, handles_color: 1, bot_color: 1,
       });
       res.sendStatus(200);
@@ -24,13 +23,14 @@ class OrderController {
   static async show(req, res) {
     try {
       const orders = await OrderEntry.findAll({
-        include: [Order, Bag, Size, Material,
+        include: [Bag, Size, Material,
           { model: Color, as: 'bagColor' },
           { model: Color, as: 'handlesColor' },
           { model: Color, as: 'botColor' },
+          { model: Order, include: [User] },
         ],
       });
-      console.log(orders);
+      // console.log(orders);
       res.render('orders/index', { orders });
     } catch (e) {
       console.log(e);
